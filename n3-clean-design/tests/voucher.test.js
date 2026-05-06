05384676a09ce9c399f45d753594b8f4182c136f
Maria Laimer <mariaeduardamouralaimer@gmail.com>
2026-05-06 14:02:51 -0300

    test(voucher.test.js): Adição/Correção de testes de vouchers de acordo com Test-Driven Design

--- n3-clean-design/tests/voucher.test.js
+++ n3-clean-design/tests/voucher.test.js
@@ -1,8 +1,17 @@
 import { calcularDesconto } from '../src/calculadoraVoucher.js';
 
 describe('VoucherCalculator (RED State)', () => {
-  test('deve aplicar 10% para o voucher NATAL10', () => {
-    expect(calcularDesconto('NATAL10', 100)).toBe(10);
+  test('não deve aplicar desconto para voucher inválido', () => {
+    expect(calcularDesconto('NATAL10', 100)).toBe(100);
   });
 });
 
+describe('VoucherCalculator (GREEN State -> REFACTOR)', () => {
+test('deve aplicar 10% para o voucher DEZ', () => {
+    expect(calcularDesconto('DEZ', 700)).toBe(630);
+  });
+
+  test('deve aplicar 30% para o voucher TRINTA', () => {
+    expect(calcularDesconto('TRINTA', 100)).toBe(70);
+  });
+});
