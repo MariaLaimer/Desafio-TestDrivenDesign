99f35f2c3e69fa102207514f089638d1e8a9aa77
Maria Laimer <mariaeduardamouralaimer@gmail.com>
2026-05-06 13:58:07 -0300

    feat(calculadoraVoucher.js): Uso de Table-Driven Design para validar cupons
    Implement discount calculation based on coupon codes.

--- n3-clean-design/src/calculadoraVoucher.js
+++ n3-clean-design/src/calculadoraVoucher.js
@@ -1,5 +1,15 @@
-export function calcularDesconto(codigo, valor) {
-  // 🚀 Implemente aqui via TDD
-  return 0;
-}
+const CUPONS = [
+  {
+    cupom: (c,v) => c === 'DEZ',
+    desconto: v => v * 0.90,
+  },
+  {
+    cupom: (c,v) => c === 'TRINTA',
+    desconto: v => v * 0.70,
+  },
+];
 
+export function calcularDesconto(codigo, valor) {
+  const cupom = CUPONS.find(c => c.cupom(codigo, valor));
+  return cupom ? cupom.desconto(valor) : valor;
+}
