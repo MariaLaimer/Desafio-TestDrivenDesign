748814e60e73a6f1a83f5a0f747a51588cb9d455
Maria Laimer <mariaeduardamouralaimer@gmail.com>
2026-05-06 10:10:47 -0300

    fix(teste do validator): Correção para que teste funcione com novas nomenclaturas

--- n1-srp/tests/validator.test.js
+++ n1-srp/tests/validator.test.js
@@ -1,9 +1,9 @@
-import { validadorUsuario } from '../src/validador.js';
+import { userValidator } from '../src/validador.js';
 
 describe('UserValidator (Initial State)', () => {
   test('deve validar um usuário correto', () => {
     const user = { email: 'test@atitus.edu.br', password: 'password123', age: 20 };
-    expect(validadorUsuario(user)).toBe(true);
+    expect(userValidator(user)).toBe(true);
   });
 });
 
